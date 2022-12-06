107f23df59e7913b19b287e0a9d3f61fe97d76d8
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2022-12-06 04:48:56 +0300

    delete non-working function

--- firmware/Core/Inc/util.h
+++ firmware/Core/Inc/util.h
@@ -3,7 +3,4 @@
 
 #include "stdint.h"
 
-void tick_counter_init();
-inline uint32_t get_tick();
-
 #endif
--- firmware/Core/Src/util.c
+++ firmware/Core/Src/util.c
@@ -1,11 +1,1 @@
 #include "util.h"
-#include "core_cm3.h"
-
-void tick_counter_init(){
-  CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
-  DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
-}
-
-inline uint32_t get_tick(){
-  return DWT->CYCCNT;
-}
